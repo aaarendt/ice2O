81f055961f1c65b67e71fa2c630b1fe43794fc08
Emily Baker <ehbaker@usgs.gov>
2017-03-02 17:43:15 -0900

    edited to load Settings from top level folder

--- backup/copyTables.py
+++ backup/copyTables.py
@@ -6,15 +6,19 @@
 #External Modules
 import os
 import pandas as pd
-import settings
-
-user = AWS_localhost['user']
+import sys
 
 # internal modules
-import DbImport 
+thisfilelocation=os.getcwd() #path for this file
+utils_folder=os.path.join(os.path.dirname(thisfilelocation), "utils")
+top_folder=os.path.join(os.path.dirname(thisfilelocation))
+sys.path.append(utils_folder) #add a folder one level up to the system path; this is where modules are stored
+sys.path.append(top_folder)
+from utils import DbImport
 from settings import *
 
 # FROM #
+user = AWS_localhost['user']
 fromdb = AWS_localhost['dbname']
 fromHost = AWS_localhost['host']
 fromPort = AWS_localhost['port']
@@ -24,10 +28,12 @@ todb = AWS_backup['dbname']
 toHost = AWS_backup['host']
 toPort = AWS_backup['port']
 
-tableList = pd.read_excelpth_tableList)
+# Import list of tables; 2 columns. Column @update will read '1' if to be updated.
+tableList = pd.read_excel(os.path.join(top_folder, tableList_nm)) #tableList_nm defined in settings file
 
 for index, row in tableList.iterrows():
     if row['update']:
+        print (row['table'])
         commandString = "pg_dump -C -h " + fromHost + " -U " + user + " -t " + row['table'] + " -p " + fromPort +  \
                     " " + fromdb + " | psql -h " + toHost + " -p " + toPort + " -U " + user + " " + todb
         os.system(commandString)
